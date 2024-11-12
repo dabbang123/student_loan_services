e89b2134d0aba4bd655c9b6438b51edf5e04eded
Abhishek Vishwakarma <abhishekvishwakarma087@gmail.com>
2024-11-12 19:58:01 +0000

    Abhishek

--- SD_StudentLoanServices/src/test/java/com/sd/sls/SDStudentLoanServicesTests.java
+++ SD_StudentLoanServices/src/test/java/com/sd/sls/SDStudentLoanServicesTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.sd.sls;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SDStudentLoanServicesTests {
 
 	@Test
 	void contextLoads() {
